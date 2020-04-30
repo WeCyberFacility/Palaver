3a4126b1e82672b78133ffe8b032da4453c14e4f
Alpay Kücük <cyberfacilitygroup@gmail.com>
2020-04-30 16:47:22 +0200

    Splashscreen fertig

--- app/src/main/java/de/cyberfacility/alpaykucuk/palaver/MainActivity.java
+++ app/src/main/java/de/cyberfacility/alpaykucuk/palaver/MainActivity.java
@@ -2,7 +2,10 @@ package de.cyberfacility.alpaykucuk.palaver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logout_save();
+        checkLoggedIn();
+    }
+
+
+
+    public void checkLoggedIn() {
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        Boolean loggedin = preferences.getBoolean("loggedIn", false);
+        System.out.println("LoggedIn: " + loggedin);
+        if(loggedin)
+        {
+            Intent myIntent = new Intent(MainActivity.this, splashActivity.class);
+            this.startActivity(myIntent);
+        }
+        else {
+
+        }
+    }
+
+
+    public void login_save() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("loggedIn",true);
+        editor.apply();
+    }
+
+    public void logout_save() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("loggedIn",false);
+        editor.apply();
     }
 }
---
+++ app/src/main/java/de/cyberfacility/alpaykucuk/palaver/splashActivity.java
@@ -0,0 +1,28 @@
+package de.cyberfacility.alpaykucuk.palaver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashActivity extends Activity {
+
+    private final int Splashscreenduration = 2300;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                //springe direkt zum hauptmenü
+
+            }
+        }, Splashscreenduration);
+
+
+    }
+}
